cb3a72375ba71cfe4e9d5933949514416d51b017
yuriisilkov <silkov.yurii@gmail.com>
2022-11-08 12:46:38 +0200

    Revert "Revert "New commit""
    This reverts commit 5c4a53f19f28c4c45dbcb71fd7d4a29114a5bc98.

---
+++ libs/no_hire.py
@@ -0,0 +1,254 @@
+NO_JOB_OFFERS_CV_ONLY = "there are no specific vacancies on the career page, only the ability to leave a CV"
+NO_JOB_OFFERS = "there are no specific vacancies on the career page"
+NOT_WORKING_SITE = "site is down"
+JOBS_OFFERS_ARE_DEEPER = "job vacancies located deeper on the site"
+JOBS_OFFERS_ARE_PRESENT = "job vacancies are present on the career page"
+NO_CAREER_PAGE = ""
+
+NOT_IT_JOBS_OFFERS_ARE_PRESENT = "not it related job vacancies are present on the career page"
+
+
+
+
+
+no_hire = {
+  "034motorsport.com": JOBS_OFFERS_ARE_PRESENT,
+  "1000sads.com": JOBS_OFFERS_ARE_DEEPER,
+  "1000ut.hu": NO_JOB_OFFERS,
+  "100limites.com.pt": NOT_WORKING_SITE,
+  "101careers.co.za": NOT_WORKING_SITE,
+  "128volvo.com": JOBS_OFFERS_ARE_DEEPER,
+  "12jars.supanet.com": NOT_WORKING_SITE,
+  "1boc.com": NOT_WORKING_SITE,
+  "1sky.org": NOT_WORKING_SITE,
+  "1smg.com": NO_JOB_OFFERS_CV_ONLY,
+  "21stcenturyhealthcare.com": NO_JOB_OFFERS_CV_ONLY,
+  "21stplc.com": NOT_WORKING_SITE,
+  "22hotel.com": NOT_WORKING_SITE,
+  "247e.com": NOT_WORKING_SITE,
+  "247ms.com": NOT_WORKING_SITE,
+  "247staff.com": NO_CAREER_PAGE,
+  "24tv.com.ua": NOT_WORKING_SITE,
+  "288group.com": NOT_WORKING_SITE,
+  "2kgames.com": JOBS_OFFERS_ARE_DEEPER,
+  "2kgroup.com": JOBS_OFFERS_ARE_PRESENT,
+  "advertisemint.com": NO_JOB_OFFERS,
+  "amaturecharms.com": NO_CAREER_PAGE,
+  "apropoesharboreast.com": JOBS_OFFERS_ARE_PRESENT,
+  "bigboldyouth.org.uk": NOT_WORKING_SITE,
+  # "careertransitionsllc.com",
+  "cincynation.com": NOT_WORKING_SITE,
+  "drnewitt.com": JOBS_OFFERS_ARE_DEEPER,
+  "ebs-consulting.com": NO_JOB_OFFERS_CV_ONLY,
+  "electromet.com": NOT_WORKING_SITE,
+  "elscintautomation.com": NO_JOB_OFFERS_CV_ONLY,
+  "eurosort.com": JOBS_OFFERS_ARE_DEEPER,
+  "fagot.ua": NOT_WORKING_SITE,
+  "good-free-porn.net": NO_CAREER_PAGE,
+  "gordonstowe.com": NOT_IT_JOBS_OFFERS_ARE_PRESENT,
+  "goscorlifttrucks.co.za": NOT_IT_JOBS_OFFERS_ARE_PRESENT,
+  "hlamovoz.kiev.ua": NO_JOB_OFFERS_CV_ONLY,
+  "ipcl.in": NO_JOB_OFFERS_CV_ONLY,
+  "jovanmusk.com": JOBS_OFFERS_ARE_DEEPER,
+  "manteksolutions.com": JOBS_OFFERS_ARE_PRESENT,
+  "mecresults.com": JOBS_OFFERS_ARE_PRESENT,
+  "mortgagewomenmagazine.com": NOT_WORKING_SITE,
+  "myfreeones.com": NO_CAREER_PAGE,
+  "numbersonly.com": JOBS_OFFERS_ARE_DEEPER,
+  "nurseryjobs.co.uk": JOBS_OFFERS_ARE_DEEPER,
+  "pnr.eu": NOT_IT_JOBS_OFFERS_ARE_PRESENT,
+  "ponticelloinc.com": NO_JOB_OFFERS,
+  "pornfiles.org": NO_CAREER_PAGE,
+  "pornmoviesonthe.net": NO_CAREER_PAGE,
+  "pornpy.com": NO_CAREER_PAGE,
+  "predaptive.com": JOBS_OFFERS_ARE_DEEPER,
+  "sextir.com": NOT_WORKING_SITE,
+  "stepin2it.com": NO_JOB_OFFERS_CV_ONLY,
+  "unesco.go.ke": NO_JOB_OFFERS,
+  "whks.com": NOT_IT_JOBS_OFFERS_ARE_PRESENT,
+  "yfiles.com": JOBS_OFFERS_ARE_PRESENT,
+  "zhajda.ru": NOT_WORKING_SITE,
+  "126ydb.com": NOT_WORKING_SITE,
+  "arenacube.com": NOT_WORKING_SITE,
+  "arla-group.com": JOBS_OFFERS_ARE_DEEPER,
+  "bamritchies.co.uk": NOT_WORKING_SITE,
+  "base10.ws": NOT_WORKING_SITE,
+  "bbwpornfilm.com": NO_CAREER_PAGE,
+  "beeple.eu": JOBS_OFFERS_ARE_DEEPER,
+  "bigtitgirls.com": NOT_WORKING_SITE,
+  "buyvisualstudionetcomponents.com": NOT_WORKING_SITE,
+  "changeaware-processes.com": JOBS_OFFERS_ARE_DEEPER,
+  "classywheeler.com.hk": JOBS_OFFERS_ARE_DEEPER,
+  "clickntax.com": NO_CAREER_PAGE,
+  "cobaltrecruitment.de": NOT_IT_JOBS_OFFERS_ARE_PRESENT,
+  "colemantech.com": JOBS_OFFERS_ARE_PRESENT,
+  "csmarine.com.sg": NO_JOB_OFFERS,
+  "dynamicic.com": NO_JOB_OFFERS,
+  "fastdownload.link": NOT_WORKING_SITE,
+  "fatsexualwomen.com": NO_CAREER_PAGE,
+  "fiorentemente.it": NO_JOB_OFFERS,
+  "fortune500software.com": NOT_WORKING_SITE,
+  "geicoclaim.com": JOBS_OFFERS_ARE_DEEPER,
+  "goplae.co.uk": NOT_WORKING_SITE,
+  "gravitylab.de": NOT_WORKING_SITE,
+  "highfieldassessment.com": NOT_IT_JOBS_OFFERS_ARE_PRESENT,
+  "hub-optimization.com": JOBS_OFFERS_ARE_DEEPER,
+  "itsbrightinhere.com": JOBS_OFFERS_ARE_DEEPER,
+  "jahreswagen.com": NOT_WORKING_SITE,
+  "jenny-cams.com": NO_CAREER_PAGE,
+  "karcher-career.com": JOBS_OFFERS_ARE_DEEPER,
+  "knowledge16.com": NO_CAREER_PAGE,
+  "kpinterface.com": JOBS_OFFERS_ARE_PRESENT,
+  "latinapornhd.com": NO_CAREER_PAGE,
+  "miaa.nhs.uk": NO_JOB_OFFERS,
+  "objectstream.com": JOBS_OFFERS_ARE_DEEPER,
+  "oneplumbplumbing.com.au": NOT_WORKING_SITE,
+  "personalizedbannerz.com": NOT_WORKING_SITE,
+  "plumpbbwmovies.com": NO_CAREER_PAGE,
+  "profesoringles.net": NOT_IT_JOBS_OFFERS_ARE_PRESENT,
+  "roingcentral.com": JOBS_OFFERS_ARE_DEEPER,
+  "schott-glas.com": JOBS_OFFERS_ARE_DEEPER,
+  "seehalsaryaengg.com": NOT_WORKING_SITE,
+  "seks-chaty.ru": NOT_WORKING_SITE,
+  "sexmixxx.com": NO_CAREER_PAGE,
+  "shemalefuckvideo.com": NOT_WORKING_SITE,
+  "silexica.com": JOBS_OFFERS_ARE_DEEPER,
+  "sourcebans.ru": NOT_WORKING_SITE,
+  "sscnow.com": JOBS_OFFERS_ARE_DEEPER,
+  "svezheemoloko.ru": NOT_WORKING_SITE,
+  "technocorp.co.in": NOT_WORKING_SITE,
+  "teenpornstreams.com": NO_CAREER_PAGE,
+  "teensexmania.tv": NO_CAREER_PAGE,
+  "textminded.be": NOT_WORKING_SITE,
+  "transunioncibil.com": NOT_WORKING_SITE,
+  "uznewmax.com": NOT_WORKING_SITE,
+  "ventureforth.com": NO_CAREER_PAGE,
+  "wildafricansex.com": NO_CAREER_PAGE,
+  "xgyulin.com": NOT_WORKING_SITE,
+  "yilportholding.com": NO_JOB_OFFERS,
+  "yuehaomicro.com": NOT_WORKING_SITE,
+  "agriculturalcuttingtools.com": NOT_WORKING_SITE,
+  "airhoppark.de": NO_CAREER_PAGE,
+  "ameetchana.com": NOT_WORKING_SITE,
+  "animesexbar.com": NO_CAREER_PAGE,
+  "appsafer.net": NOT_WORKING_SITE,
+  "bajkowyswiat.gniezno.pl": NOT_WORKING_SITE,
+  "bizer.de": NO_JOB_OFFERS_CV_ONLY,
+  "blackdogsailing.com": NOT_WORKING_SITE,
+  "brigittalee.com": NOT_WORKING_SITE,
+  "businessgeorgia.ge": NO_JOB_OFFERS_CV_ONLY,
+  "cantbeevil.com": NOT_WORKING_SITE,
+  "cherihead.com": NO_CAREER_PAGE,
+  "citylandtech.com": NOT_WORKING_SITE,
+  "congruentsolutions.com": JOBS_OFFERS_ARE_DEEPER,
+  "crazymalesex.com": NO_CAREER_PAGE,
+  "designforcomplexity.com": NOT_WORKING_SITE,
+  "dinguedemusique.com": NO_CAREER_PAGE,
+  # "faceboonk.com": ,
+  "facesurgery.org": NOT_WORKING_SITE,
+  "filmepornoxxx.club": NO_CAREER_PAGE,
+  "firstdataglobalgateway.com": NOT_WORKING_SITE,
+  "fluorlogcaprecruiters.com": JOBS_OFFERS_ARE_DEEPER,
+  "fluorsupplychainsolutions.com": NOT_WORKING_SITE,
+  "freexxxporn.tv": NO_CAREER_PAGE,
+  "freitagreference.com": NOT_IT_JOBS_OFFERS_ARE_PRESENT,
+  "friendsofthermalcooking.com": NOT_WORKING_SITE,
+  "fucktube.sexy": NO_CAREER_PAGE,
+  "gb2megion.ru": NOT_WORKING_SITE,
+  "giantrecruiting.com": NOT_WORKING_SITE,
+  "golf-safari.com": NO_JOB_OFFERS,
+  "grossum.com": NOT_WORKING_SITE,
+  "hdrtranslationalhealthscience.com": NOT_WORKING_SITE,
+  "hdrtranslationalhealthsciences.com": NOT_WORKING_SITE,
+  "hempchica.com": NO_CAREER_PAGE,
+  "hotgoldenshower.com": NO_CAREER_PAGE,
+  "htultraglass.com": JOBS_OFFERS_ARE_DEEPER,
+  "inescudna4free.com": NO_CAREER_PAGE,
+  "jbhtesla.com": NOT_WORKING_SITE,
+  "jobspot.lk": NOT_WORKING_SITE,
+  "jobticket.de": NO_CAREER_PAGE,
+  "klickerklacker.com": JOBS_OFFERS_ARE_PRESENT,
+  "kljb-schermbeck.org": NOT_WORKING_SITE,
+  "labtechessa.com": JOBS_OFFERS_ARE_PRESENT,
+  "lararaavisfilms.com": NOT_WORKING_SITE,
+  "lunchalytics.com": NOT_WORKING_SITE,
+  "maritime-software.com": NOT_WORKING_SITE,
+  "metserv.info": NOT_WORKING_SITE,
+  "modernisierung-weiss.com": NOT_IT_JOBS_OFFERS_ARE_PRESENT,
+  "mumbaikhoj.com": NOT_WORKING_SITE,
+  "naykedapparel.com": NO_JOB_OFFERS,
+  "nnplanet.pw": NOT_WORKING_SITE,
+  "oxfordsummerabroad.com": NO_JOB_OFFERS_CV_ONLY,
+  "premiermill.com": NOT_WORKING_SITE,
+  "professional-ic.com": NOT_WORKING_SITE,
+  "prolog-italia.com": NOT_WORKING_SITE,
+  "rezumesolutions.com": NO_CAREER_PAGE,
+  "sharepointavenue.com": NOT_WORKING_SITE,
+  "skillgameswiki.com": JOBS_OFFERS_ARE_PRESENT,
+  "skillsdevelopmentscotlandlimited.com": NO_JOB_OFFERS,
+  "splunknova.com": NOT_WORKING_SITE,
+  "storck-travel-retail.com": JOBS_OFFERS_ARE_DEEPER,
+  "sweetgrannysex.com": NO_CAREER_PAGE,
+  "thesilentdescendents.com": NOT_WORKING_SITE,
+  "trsstaffingsolutionsfluor.com": NOT_IT_JOBS_OFFERS_ARE_PRESENT,
+  "uebergangsregierung.com": NOT_WORKING_SITE,
+  "unicosearch.com": NOT_WORKING_SITE,
+  "usfmall.com": NOT_WORKING_SITE,
+  "utabusinessalumni.com": NO_JOB_OFFERS,
+  "uxibus.com": NOT_WORKING_SITE,
+  "vitraux-lyon.com": NO_CAREER_PAGE,
+  "wcbvalves.com": JOBS_OFFERS_ARE_DEEPER,
+  "xporno.me": NO_CAREER_PAGE,
+  "youpornwebcams.com": NO_CAREER_PAGE,
+  "zhouguangqing.com": NOT_WORKING_SITE
+}
+
+NO_JOB_OFFERS_CV_ONLY_LIST = []
+NO_JOB_OFFERS_LIST = []
+NOT_WORKING_SITE_LIST = []
+JOBS_OFFERS_ARE_DEEPER_LIST = []
+JOBS_OFFERS_ARE_PRESENT_LIST = []
+NO_CAREER_PAGE_LIST = []
+NOT_IT_JOBS_OFFERS_ARE_PRESENT_LIST = []
+
+for i in no_hire:
+  if no_hire[i] == NO_JOB_OFFERS_CV_ONLY:
+    NO_JOB_OFFERS_CV_ONLY_LIST.append(i)
+  if no_hire[i] == NO_JOB_OFFERS:
+    NO_JOB_OFFERS_LIST.append(i)
+  if no_hire[i] == NOT_WORKING_SITE:
+    NOT_WORKING_SITE_LIST.append(i)
+  if no_hire[i] == JOBS_OFFERS_ARE_DEEPER:
+    JOBS_OFFERS_ARE_DEEPER_LIST.append(i)
+  if no_hire[i] == JOBS_OFFERS_ARE_PRESENT:
+    JOBS_OFFERS_ARE_PRESENT_LIST.append(i)
+  if no_hire[i] == NO_CAREER_PAGE:
+    NO_CAREER_PAGE_LIST.append(i)
+  if no_hire[i] == NOT_IT_JOBS_OFFERS_ARE_PRESENT:
+    NOT_IT_JOBS_OFFERS_ARE_PRESENT_LIST.append(i)
+
+
+def percentage(part):
+  whole = len(no_hire)
+  percentage = str(100 * part/whole).split('.')[0]
+  return str(percentage) + " %"
+
+
+print(f"All 'NO Hire' sites: {len(no_hire)}")
+
+
+print(f'NO_JOB_OFFERS_CV_ONLY: {percentage(len(NO_JOB_OFFERS_CV_ONLY_LIST))}')
+print(f'NO_JOB_OFFERS: {percentage(len(NO_JOB_OFFERS_LIST))}')
+print(f'NOT_WORKING_SITE: {percentage(len(NOT_WORKING_SITE_LIST))}')
+print(f'JOBS_OFFERS_ARE_DEEPER: {percentage(len(JOBS_OFFERS_ARE_DEEPER_LIST))}')
+print(f'JOBS_OFFERS_ARE_PRESENT: {percentage(len(JOBS_OFFERS_ARE_PRESENT_LIST))}')
+print(f'NO_CAREER_PAGE: {percentage(len(NO_CAREER_PAGE_LIST))}')
+print(f'NOT_IT_JOBS_OFFERS_ARE_PRESENT: {percentage(len(NOT_IT_JOBS_OFFERS_ARE_PRESENT_LIST))}')
+
+
+
+
+
+
+
+
